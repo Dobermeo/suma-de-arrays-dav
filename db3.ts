56767f38b3322d592605668c0a4bed15d3ab82c5
David <dobermeo@sudamericano.edu.ec>
2022-11-27 13:04:56 -0500

    Pasar FUNCION FLECHA Y ANONIMA

--- db3.ts
+++ db3.ts
@@ -8,3 +8,16 @@ function sumar(...valores:number[]) {
   console.log(sumar(10, 2, 44, 3));
   console.log(sumar(1, 2));
   console.log(sumar());
+
+  //pasar a funcion flecha
+  let suma = (valores:number[]) => {
+    let sum=0;
+    for(let x=0;x<valores.length;x++)
+      sum+=valores[x];
+    return sum;
+  }
+
+  //pasar a funcion anonima 
+  document.getElementById("ejemplo")!.addEventListener("click", function(){
+    console.log("ejemplo clicked");
+  })
